0742ca9c5d93470ace0dc47ad2430d56acffbae5
lorenzo-senettin <senettin.lorenzo54@gmail.com>
2025-02-24 16:48:06 +0100

    add second snack

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -5,9 +5,23 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
 // Dichiara la funzione qui.
 
+function inLett(arr) {
+
+  const firstLett = []
+
+  for (let i = 0; i < arr.length; i++) {
+    const thisElement = arr[i];
+
+    firstLett.push(thisElement.charAt(0))
+
+  }
+
+  return firstLett;
+}
+
 
 // Invoca la funzione qui e stampa il risultato in console
-
-
+const firstLetter = inLett(names);
+console.log(firstLetter);
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
