092d110707863343014309235d4890df6770f9e6
lorenzo-senettin <senettin.lorenzo54@gmail.com>
2025-02-24 16:45:17 +0100

    first commit

---
+++ 01_saluti/script.js
@@ -0,0 +1,16 @@
+/* Scrivi una funzione che accetti una stringa contenente un nome e restituisca un saluto seguito dal nome fornito (es. "Ciao [nome]") */
+
+const userName = 'Mario';
+
+
+// Dichiara la funzione qui.
+function hiFun(name) {
+
+  return `ciao ${name}`;
+}
+
+// Invoca la funzione qui e stampa il risultato in console
+const hiCon = hiFun(userName)
+
+//Risultato atteso se si passa 'Mario': // ciao Mario
+console.log(hiCon);
