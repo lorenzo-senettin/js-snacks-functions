8482577790e75e264803d104e541feacd957bed9
lorenzo-senettin <senettin.lorenzo54@gmail.com>
2025-02-24 16:56:41 +0100

    add snack 4

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -4,10 +4,28 @@ const word = 'javascript';
 
 
 // Dichiara la funzione qui.
+function numVoc(str) {
+
+  let count = 0;
+  const voc = ['a', 'e', 'i', 'o', 'u'];
+  const foundVowels = [];
+  for (let i = 0; i < str.length; i++) {
+    const thisChar = str[i];
+
+    if (voc.includes(thisChar)) {
+      count++;
+      foundVowels.push(thisChar);
+    }
+
+  }
+  return [count, foundVowels];
+
+}
 
 
 // Invoca la funzione qui e stampa il risultato in console
-
+const vocNumb = numVoc(word);
+console.log(vocNumb, word);
 
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
